e25ca3eca2b03eff59f534e9eff4164e5cf292ef
veerlakshya <lakshya.singh@flamapp.com>
2025-10-16 02:52:02 +0530

    ():command line subs

---
+++ intermediate/command_line_arguments.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func cmdLineArguments() {
+
+	fmt.Println("command:", os.Args)
+
+	// Define flags
+	var name string
+	var age int
+	var male bool
+
+	flag.StringVar(&name, "name", "john", "Name of the user")
+	flag.IntVar(&age, "age", 18, "Age of the user")
+	flag.BoolVar(&male, "male", true, "")
+
+	flag.Parse()
+
+	fmt.Println("Name: ", name)
+	fmt.Println("Age: ", age)
+	fmt.Println("Male: ", male)
+
+}
+
+// ussage - go run command_line_arguments.go -name Jane Doe -age 14
+
+// --help to get them all printed in terminal
---
+++ intermediate/command_line_sub_commands.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func subcommands() {
+
+	stringFlag := flag.String("user", "guest", "Name of the user")
+	flag.StringVar(stringFlag, "user", "John", "Name of user")
+	sub1 := flag.NewFlagSet("firstSub", flag.ExitOnError)
+	sub2 := flag.NewFlagSet("secondSub", flag.ExitOnError)
+
+	firstFlag := sub1.Bool("processing", false, "command processing status")
+	secondFlag := sub1.Int("bytes", 1024, "Byte length of result")
+
+	flagsc2 := sub2.String("language", "Go", "Enter Your Language")
+
+	if len(os.Args) < 2 {
+		fmt.Println("This Program requires additional commands")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "firstSub":
+		sub1.Parse(os.Args[2:])
+		fmt.Println("sub1:")
+		fmt.Println("processing:", *firstFlag)
+		fmt.Println("bytes:", *secondFlag)
+	case "secondSub":
+		sub2.Parse(os.Args[2:])
+		fmt.Println("subCommand2: ")
+		fmt.Println("language: ", flagsc2)
+	default:
+		fmt.Println("no subCommands entered")
+		os.Exit(1)
+	}
+}
