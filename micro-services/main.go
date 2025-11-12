e69c898f807f6ba6f61bb99aaed1c692de99dcc5
veerlakshya <lakshya.singh@flamapp.com>
2025-11-12 16:16:18 +0530

    feat: microservices architecture

---
+++ micro-services/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+	"my-go-files/micro-services/handlers"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
+
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal("Error starting server: ", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("Recieved terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), time.Second*30)
+
+	s.Shutdown(tc)
+}
