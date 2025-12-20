2f70ec05d3026825e1de3611dd6f0d3aff7241c9
veerlakshya <lakshya.singh@flamapp.com>
2025-12-21 04:29:12 +0530

    feat: certificates

--- apis/server.go
+++ apis/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 func main() {
@@ -16,9 +19,35 @@ func main() {
 	})
 	port := 3000
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// load the TLS cert and key
+	cert := "cert.pem"
+	key := "key.pem"
+
+	// configure TLS
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	// create a custom server
+	server := &http.Server{
+		Addr:      fmt.Sprintf(":%d", port),
+		Handler:   nil,
+		TLSConfig: tlsConfig,
+	}
+
+	// Enable http2
+	http2.ConfigureServer(server, &http2.Server{})
+
+	log.Printf("server is running on port: %d", port)
+	err := server.ListenAndServeTLS(cert, key)
 	if err != nil {
 		log.Fatalln("Could not start server: ", err)
 	}
-	log.Println("server is running on port: ", port)
+
+	// === HTTP 1.1 Server without TLS
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// if err != nil {
+	// 	log.Fatalln("Could not start server: ", err)
+	// }
+	// log.Println("server is running on port: ", port)
 }
